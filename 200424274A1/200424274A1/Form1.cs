d6dfea271cfb89ea96b6f871a5e5282047bce264
Taeler Burgess <200424274@student.georgianc.on.ca>
2020-02-23 15:22:11 -0500

    Calculations and Comments
    Calculations have been done, and comments have been added.

--- 200424274A1/200424274A1/Form1.cs
+++ 200424274A1/200424274A1/Form1.cs
@@ -19,6 +19,8 @@ namespace _200424274A1
 
         private void calcbutton_Click(object sender, EventArgs e)
         {
+            //intialization of the variables
+
             double total = 0;
 
             double paid = 0;
@@ -32,10 +34,60 @@ namespace _200424274A1
             int dimes = 0;
 
             int nickles = 0;
+
+
+            //total is stored in this textbox
+            total = double.Parse(textBox1.Text);
+
+            //paid is stored in this textbox
+            paid = double.Parse(paidBox.Text);
+
+            //change box holds the diffrence of paid and total
+            changeBox.Text = (paid - total).ToString();
+            double change = (paid - total) * 100; 
+
+            //to find the toonies, divide change by 200 and get the remainder
+            toonie = (int)change / 200;
+            tooniesBox.Text = toonie.ToString();
+            change = change % 200;
+
+            //to find the loonies, divide change by 100 and get the remainder
+            loonie = (int)change / 100;
+            looniesBox.Text = loonie.ToString();
+            change = change % 100;
+
+            //to find the quarters, divide change by 25 and get the remainder
+            quarters = (int)change / 25;
+            quartersBox.Text = quarters.ToString();
+            change = change % 25;
+
+            //to find the dimes, divide change by 10 and get the remainder
+            dimes = (int)change / 10;
+            change = change % 10;
+
+            //to find the toonies, divide change by 200 and get the remainder
+            nickles = (int)change / 5;
+            change = change % 5;
+
+            //if statement to determine the proper change distribution between dimes and nickels
+            if (change >= 3)
+            {
+                nickles++;
+            } //if change is greater than 3, round up
+
+            if(nickles == 2) 
+            {
+                nickles = 0; dimes++;
+            } //if nickles are 2, convert it to a dime
+
+            dimesBox.Text = dimes.ToString(); //Holds the dime info in this textbox
+            nickelsBox.Text = nickles.ToString(); //Holds the nickles info in this textbox
         }
 
         private void clearButton_Click(object sender, EventArgs e)
         {
+            // Initializing textboxes
+
             textBox1.Text = "";
 
             paidBox.Text = "";
@@ -50,7 +102,7 @@ namespace _200424274A1
 
             dimesBox.Text = "";
 
-            nickelsBox.Text = "";
+            nickelsBox.Text = ""; 
         }
     }
 }
